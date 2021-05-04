df77bdb5d82c112d7f8c6a26766e1b766572dc3b
Ersan Karimi <73420137+ersankarimi@users.noreply.github.com>
2021-05-04 15:00:55 +0800

    Delete tugas.builder directory

--- tugas.builder/src/Builder/KulinerBuilder.java
+++
@@ -1,8 +0,0 @@
-package Builder;
-
-public interface KulinerBuilder {
-    public void DaerahBuilder();
-    public void MakananBuilder();
-    public void MinumanBuilder();
-    public Kuliner GetKuliner();
-}
